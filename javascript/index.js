2ff7452ceb31a5e7b8e3d2b8bb8b97ce1ec1e2dc
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-04-22 13:19:28 -0500

    javascript funcionando

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,38 @@
 let button = document.getElementById("for_button")
 let card = document.getElementsByClassName("card")[0]
 let card_Active = document.getElementsByClassName("card-active")[0]
+let number = document.getElementsByClassName("number")[0]
 
-console.log(button)
-console.log(card)
+let select = 0 ;
+
+function holi (){
+    if(document.getElementById("option_one").checked){
+        select = 1
+    }
+    if(document.getElementById("option_two").checked){
+        select = 2
+    }
+    if(document.getElementById("option_three").checked){
+        select = 3
+    }
+    if(document.getElementById("option_four").checked){
+        select = 4
+    }
+    if(document.getElementById("option_five").checked){
+        select = 5
+    }
+    
+
+    return select
+}
+
 
 button.addEventListener("click" , (e) => {
     e.preventDefault();
-
+   holi();
+   number.innerHTML = select;
    card.style.display = "none";
    card_Active.style.display ="flex"
+   
+
 })
